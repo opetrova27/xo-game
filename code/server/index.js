6e78363dff1867231fcbe48e42e0a9bc6f8e9db1
Olga <ozzeye@github.com>
2018-02-17 18:31:46 +0300

    Helmet

--- code/server/index.js
+++ code/server/index.js
@@ -2,38 +2,44 @@
 
 const
     express = require('express'),
+    helmet = require('helmet'),
     bodyParser = require('body-parser');
 
 
-module.exports = function() {
-  let server = express(),
-      create,
-      start;
+module.exports = function () {
+    let server = express(),
+        create,
+        start;
 
-  create = function(config) {
-      let routes = require('./routes');
+    create = function (config) {
+        let routes = require('./routes');
 
-      server.set('env', config.env);
-      server.set('port', config.port);
-      server.set('hostname', config.hostname);
+        server.set('env', config.env);
+        server.set('port', config.port);
+        server.set('hostname', config.hostname);
 
-      server.use(bodyParser.urlencoded({ extended: true }));
-      server.use(bodyParser.json());
+        server.use(bodyParser.urlencoded({ extended: true }));
+        server.use(bodyParser.json());
 
-      routes.init(server);
-  };
+        //use helmet for secure app
+        server.use(helmet());
+        server.use(helmet.noCache())
+        server.use(helmet.frameguard())
 
-  start = function() {
-      let hostname = server.get('hostname'),
-          port = server.get('port');
+        routes.init(server);
+    };
 
-      server.listen(port, function () {
-          console.log('Express server listening on - http://' + hostname + ':' + port);
-      });
-  };
+    start = function () {
+        let hostname = server.get('hostname'),
+            port = server.get('port');
 
-  return {
-      create: create,
-      start: start
-  };
+        server.listen(port, function () {
+            console.log('Express server listening on - http://' + hostname + ':' + port);
+        });
+    };
+
+    return {
+        create: create,
+        start: start
+    };
 };
